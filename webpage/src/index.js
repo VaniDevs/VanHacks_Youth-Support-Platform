74bf4d395c7af0f5e451495bb1229716cb3a2b08
wxy325 <wxy325@qq.com>
2018-09-22 15:11:20 -0700

    remove prime

--- webpage/src/index.js
+++ webpage/src/index.js
@@ -9,9 +9,6 @@ import AppView from './pages/AppView'
 import reducers from './reducers';
 import NetworkSaga from './sagas/NetworkSaga';
 import createSagaMiddleware from 'redux-saga';
-import 'primereact/resources/themes/nova-light/theme.css';
-// import 'primereact/resources/primereact.min.css';
-// import 'primeicons/primeicons.css';
 
 const sagaMiddleware = createSagaMiddleware();
 
