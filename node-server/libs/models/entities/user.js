50eeed9df693797b47f39594c3d1208bce51a24a
wxy325 <wxy325@qq.com>
2018-09-22 17:32:49 -0700

    Adjust user model

--- node-server/libs/models/entities/user.js
+++ node-server/libs/models/entities/user.js
@@ -2,27 +2,39 @@ const mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     async = require('async')
 
-// const log = require('../runtime/log')
-
-
-
 /**
  * @class models/user
  * @since 2.0.0
  * @description User
  */
 var schema = Schema({
-  user_id: String,
   username: String,
   /** @memberof models/user.prototype */
   password: { type: String, select: false },
   name: String, //
-  gender: String,
   email: String,
   phone: String,
   birth_date: Date,
-  education_level: Number, // 0: Pre-school 1:Primary School 2: Middle School 3: High School 4:College 5: Not educated
   address: String,
+  type: Number, // 0 teen, 1 volunteer, 2 org
+  teenInfo: {
+    gender: String,
+    education_level: Number, // 0: Pre-school 1:Primary School 2: Middle School 3: High School 4:College 5: Not educated
+    low_income: Number, // 1. Confirmed 2. Not confirmed
+    school: String,
+    field: Number, //1. Scholarship 2.Mentor 3. Council 4.Food 5.Health 6. Sport
+  },
+  volunteerInfo: {
+    gender: String,
+    education_level: Number, // 0: Pre-school 1:Primary School 2: Middle School 3: High School 4:College 5: Not educated
+    field:[number], // 1. Administration/office work 2. Coaching/mentoring 3. Technical 4. Counselling/Listening 5. Events/Stewarding
+    available:[Object],
+  },
+  organizationInfo: {
+    if_registered: Boolean,
+    location: String,
+    web: String,
+  }
 }, {
   timestamps: true
 })
@@ -32,26 +44,6 @@ const GENDER_FEMALE = 'FEMALE'
 schema.statics.GENDER_MALE = GENDER_MALE
 schema.statics.GENDER_FEMALE = GENDER_FEMALE
 
-
-/*
-// Validate duplicated
-schema.pre('save', function (next) {
-  const doc = this
-  if (!doc.isNew) {
-    const criteria = {}
-    criteria._id = { $ne: doc._id }
-    User.count(criteria, (err, count) => {
-      if (count > 0) {
-        next(new Error('该用户已存在'))
-      } else {
-        next()
-      }
-    })
-  } else {
-    next()
-  }
-})
-*/
 
 
 module.exports = mongoose.model('user', schema);
