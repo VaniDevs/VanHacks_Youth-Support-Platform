0173d4283386e6a47a1cb47a5b23b7f21177ae52
wxy325 <wxy325@qq.com>
2018-09-23 08:14:33 -0700

    Add style for app header

--- vanhack-primereact/src/components/AppHeader.js
+++ vanhack-primereact/src/components/AppHeader.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import NavigationItems from './NavigationItems';
 import './AppHeader.css';
+import {Link} from 'react-router-dom';
 
 export default class AppHeader extends Component {
   render() {
     return (
-        <div id="layout-topbar">
-          Youth Support Resource Search
-          <span>
+        <div id="layout-topbar" className="app-header">
+          <Link to="/" className="app-title">
+            <span >
+              Youth Support Resource Search
+            </span>
+          </Link>
+
+          <span
+              className="nav-item"
+          >
                 <NavigationItems
                     user={this.props.user}
                     onClickLogout={this.props.onClickLogout}
