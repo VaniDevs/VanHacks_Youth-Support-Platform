d07fd77a1e94a2485e924c19a1c58947445122d5
Xiaojing <jojoxiaojing@icloud.com>
2018-09-23 13:01:12 -0700

    Fix

--- vanhack-primereact/src/pages/search/ResultRow.js
+++ vanhack-primereact/src/pages/search/ResultRow.js
@@ -7,9 +7,14 @@ export default class ResultRow extends Component {
     const {resource} = this.props;
     return (
         <div className="search-result-row">
-          <Link to={"/program/detail/" + resource._id}><div>{resource.name}</div></Link>
-          <div>{resource.desc}</div>
-          <div className='search-result-divider'></div>
+          <div className={"title"}>
+            <Link to={"/program/detail/" + resource._id}><div>{resource.name}</div></Link>
+          </div>
+          <div className={"search-column"}>{resource.desc}</div>
+          <div className={"search-column"}>{resource.location}</div>
+          <div className={"search-column"}>{resource.deadline.substr(0, 10)}</div>
+          <div className={"search-column"}>{resource.field}</div>
+
         </div>
     );
   }
